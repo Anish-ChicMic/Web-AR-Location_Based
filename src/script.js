4ba04c8400f2fab833368bbd7ada49ccd8c80727
Anish Kumar <anish.kumar@chicmic.co.in>
2023-07-31 18:21:07 +0530

    Added: Object Tracking

--- src/script.js
+++ src/script.js
@@ -3,8 +3,9 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
 import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper.js'
 // import { THREEx } from "./ThreeX/ar-threex.js";
-import * as THREEx from '@ar-js-org/ar.js/three.js/build/ar-threex-location-only'
-
+// import * as THREEx from '@ar-js-org/ar.js/three.js/build/ar-threex-location-only'
+import * as THREEx from '@ar-js-org/ar.js/three.js/build/ar-threex'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 // /**
 //  * Base
@@ -180,62 +181,235 @@ import * as THREEx from '@ar-js-org/ar.js/three.js/build/ar-threex-location-only
 // tick()
 
 
+// ************************************************************************************************************
+// function main() {
+//     const canvas = document.querySelector('canvas.webgl')
+//     console.log("dafkf", THREE, THREEx);
+//     const scene = new THREE.Scene();
+//     const camera = new THREE.PerspectiveCamera(60, 1.33, 0.1, 10000);
+//     const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 
-function main() {
-    const canvas = document.querySelector('canvas.webgl')
-    console.log("dafkf", THREE, THREEx);
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(60, 1.33, 0.1, 10000);
-    const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+//     const arjs = new THREEx.LocationBased(scene, camera);
+//     const cam = new THREEx.WebcamRenderer(renderer);
 
-    const arjs = new THREEx.LocationBased(scene, camera);
-    const cam = new THREEx.WebcamRenderer(renderer);
+//     const geom = new THREE.BoxGeometry(20, 20, 20);
+//     const mtl = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 
-    const geom = new THREE.BoxGeometry(20, 20, 20);
-    const mtl = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+//     const deviceOrientationControls = new THREEx.DeviceOrientationControls(camera);
 
-    const deviceOrientationControls = new THREEx.DeviceOrientationControls(camera);
+//     let fetched = false;
 
-    let fetched = false;
+//     // Handle the "gpsupdate" event on the LocationBased object
+//     // This triggers when a GPS update (from the Geolocation API) occurs
+//     // 'pos' is the position object from the Geolocation API.
 
-    // Handle the "gpsupdate" event on the LocationBased object
-    // This triggers when a GPS update (from the Geolocation API) occurs
-    // 'pos' is the position object from the Geolocation API.
+//     arjs.on("gpsupdate", async (pos) => {
+//         if (!fetched) {
+//             const response = await fetch(`https://hikar.org/webapp/map?bbox=${pos.coords.longitude - 0.01},${pos.coords.latitude - 0.01},${pos.coords.longitude + 0.01},${pos.coords.latitude + 0.01}&layers=poi&outProj=4326`);
 
-    arjs.on("gpsupdate", async (pos) => {
-        if (!fetched) {
-            const response = await fetch(`https://hikar.org/webapp/map?bbox=${pos.coords.longitude - 0.01},${pos.coords.latitude - 0.01},${pos.coords.longitude + 0.01},${pos.coords.latitude + 0.01}&layers=poi&outProj=4326`);
+//             const geojson = await response.json();
 
-            const geojson = await response.json();
+//             geojson.features.forEach(feature => {
+//                 const box = new THREE.Mesh(geom, mtl);
+//                 arjs.add(box, feature.geometry.coordinates[0], feature.geometry.coordinates[1]);
+//             });
 
-            geojson.features.forEach(feature => {
-                const box = new THREE.Mesh(geom, mtl);
-                arjs.add(box, feature.geometry.coordinates[0], feature.geometry.coordinates[1]);
-            });
+//             fetched = true;
+//         }
+//     });
 
-            fetched = true;
-        }
-    });
+//     arjs.startGps();
 
-    arjs.startGps();
+//     requestAnimationFrame(render);
 
-    requestAnimationFrame(render);
+//     function render() {
+//         if (canvas.width != canvas.clientWidth || canvas.height != canvas.clientHeight) {
+//             renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
+//             const aspect = canvas.clientWidth / canvas.clientHeight;
+//             camera.aspect = aspect;
+//             camera.updateProjectionMatrix();
+//         }
 
-    function render() {
-        if (canvas.width != canvas.clientWidth || canvas.height != canvas.clientHeight) {
-            renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
-            const aspect = canvas.clientWidth / canvas.clientHeight;
-            camera.aspect = aspect;
-            camera.updateProjectionMatrix();
-        }
+//         // Update the scene using the latest sensor readings
+//         deviceOrientationControls.update();
 
-        // Update the scene using the latest sensor readings
-        deviceOrientationControls.update();
+//         cam.update();
+//         renderer.render(scene, camera);
+//         requestAnimationFrame(render);
+//     }
+// }
 
-        cam.update();
-        renderer.render(scene, camera);
-        requestAnimationFrame(render);
+// main();
+
+// ************************************************************************************************************
+
+
+//////////////////////////////////////////////////////////////////////////////////
+//		Init
+//////////////////////////////////////////////////////////////////////////////////
+
+var renderer = new THREE.WebGLRenderer({
+    antialias: true,
+    alpha: true,
+    precision: 'mediump',
+});
+
+var clock = new THREE.Clock();
+
+var mixers = [];
+
+renderer.setPixelRatio(window.devicePixelRatio);
+
+renderer.setClearColor(new THREE.Color('lightgrey'), 0)
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.domElement.style.position = 'absolute'
+renderer.domElement.style.top = '0px'
+renderer.domElement.style.left = '0px'
+document.body.appendChild(renderer.domElement);
+
+// init scene and camera
+var scene = new THREE.Scene();
+
+//////////////////////////////////////////////////////////////////////////////////
+//		Initialize a basic camera
+//////////////////////////////////////////////////////////////////////////////////
+
+// Create a camera
+var camera = new THREE.Camera();
+scene.add(camera);
+
+var light = new THREE.AmbientLight(0xffffff);
+scene.add(light);
+
+////////////////////////////////////////////////////////////////////////////////
+//          handle arToolkitSource
+////////////////////////////////////////////////////////////////////////////////
+
+var arToolkitSource = new THREEx.ArToolkitSource({
+    sourceType: 'webcam',
+    sourceWidth: 480,
+    sourceHeight: 640,
+})
+
+arToolkitSource.init(function onReady() {
+    // use a resize to fullscreen mobile devices
+    setTimeout(function () {
+        onResize()
+    }, 1000);
+})
+
+// handle resize
+window.addEventListener('resize', function () {
+    onResize()
+})
+
+// listener for end loading of NFT marker
+window.addEventListener('arjs-nft-loaded', function (ev) {
+    console.log(ev);
+})
+
+function onResize() {
+    arToolkitSource.onResizeElement()
+    arToolkitSource.copyElementSizeTo(renderer.domElement)
+    if (arToolkitContext.arController !== null) {
+        arToolkitSource.copyElementSizeTo(arToolkitContext.arController.canvas)
     }
 }
 
-main();
+////////////////////////////////////////////////////////////////////////////////
+//          initialize arToolkitContext
+////////////////////////////////////////////////////////////////////////////////
+
+// create atToolkitContext
+var arToolkitContext = new THREEx.ArToolkitContext({
+    detectionMode: 'mono',
+    canvasWidth: 480,
+    canvasHeight: 640,
+}, {
+    sourceWidth: 480,
+    sourceHeight: 640,
+})
+
+// initialize it
+arToolkitContext.init(function onCompleted() {
+    // copy projection matrix to camera
+    camera.projectionMatrix.copy(arToolkitContext.getProjectionMatrix());
+})
+
+////////////////////////////////////////////////////////////////////////////////
+//          Create a ArMarkerControls
+////////////////////////////////////////////////////////////////////////////////
+
+// init controls for camera
+var markerControls = new THREEx.ArMarkerControls(arToolkitContext, camera, {
+    type: 'nft',
+    descriptorsUrl: 'data/dataNFT/pinball',
+    changeMatrixMode: 'cameraTransformMatrix'
+})
+
+scene.visible = false
+
+var root = new THREE.Object3D();
+scene.add(root);
+
+//////////////////////////////////////////////////////////////////////////////////
+//		add an object in the scene
+//////////////////////////////////////////////////////////////////////////////////
+
+var threeGLTFLoader = new GLTFLoader();
+var model;
+
+threeGLTFLoader.load("./assets/Flamingo.glb", function (gltf) {
+    model = gltf.scene.children[0];
+    model.name = 'Flamingo';
+    const clips = gltf.animations;
+
+    var animation = gltf.animations[0];
+    var mixer = new THREE.AnimationMixer(gltf.scene);
+    mixers.push(mixer);
+    const clip = THREE.AnimationClip.findByName(clips, 'flamingo_flyA_');
+    var action = mixer.clipAction(clip);
+    action.play();
+
+    root.matrixAutoUpdate = false;
+    root.add(model);
+
+    model.position.z = -100;
+    //model.position.z = 100;
+
+    window.addEventListener('arjs-nft-init-data', function (nft) {
+        console.log(nft);
+        var msg = nft.detail;
+        model.position.y = (msg.height / msg.dpi * 2.54 * 10) / 2.0; //y axis?
+        model.position.x = (msg.width / msg.dpi * 2.54 * 10) / 2.0; //x axis?
+    })
+
+
+    //////////////////////////////////////////////////////////////////////////////////
+    //		render the whole thing on the page
+    //////////////////////////////////////////////////////////////////////////////////
+
+    var animate = function () {
+        requestAnimationFrame(animate);
+
+        if (mixers.length > 0) {
+            for (var i = 0; i < mixers.length; i++) {
+                mixers[i].update(clock.getDelta());
+            }
+        }
+
+        if (!arToolkitSource.ready) {
+            return;
+        }
+
+        arToolkitContext.update(arToolkitSource.domElement)
+
+        // update scene.visible if the marker is seen
+        scene.visible = camera.visible;
+
+        renderer.render(scene, camera);
+    };
+
+    requestAnimationFrame(animate);
+}
+);
