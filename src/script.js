099aa4907914601dccd6bb7241015ad649e2fa8f
Anish Kumar <anish.kumar@chicmic.co.in>
2023-08-01 12:13:30 +0530

    Debugging: rottation test

--- src/script.js
+++ src/script.js
@@ -381,8 +381,8 @@ threeGLTFLoader.load("./Flamingo.glb", function (gltf) {
         console.log("hey:", msg);
         model.position.y = (msg.height / msg.dpi * 2.54 * 10) / 2.0; //y axis?
         model.position.x = (msg.width / msg.dpi * 2.54 * 10) / 2.0; //x axis?
-        model.rotation.x += 40;
-        model.rotation.y += 40;
+        model.rotation.x += 20;
+        model.rotation.y += 20;
         model.rotation.z += 40;
         camera.lookAt(model.position);
         var heading = document.getElementById('heading');
