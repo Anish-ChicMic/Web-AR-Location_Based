d2261e8c1f04500c000c9a9d9b6ae1f8fef7d41e
Anish Kumar <anish.kumar@chicmic.co.in>
2023-08-02 12:24:04 +0530

    Added 3d model

--- src/liveLoaction.js
+++ src/liveLoaction.js
@@ -130,17 +130,19 @@ function main() {
 
     function setupObjects(longitude, latitude) {
         // Use position of first GPS update (fake or real)
-        // const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-        // const material2 = new THREE.MeshBasicMaterial({ color: 0xffff00 });
-        // const material3 = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-        // const material4 = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        // arjs.add(new THREE.Mesh(geom, material), longitude, latitude + 0.001); // slightly north
-        // arjs.add(new THREE.Mesh(geom, material2), longitude, latitude - 0.001); // slightly south
-        // arjs.add(new THREE.Mesh(geom, material3), longitude - 0.001, latitude); // slightly west
-        // arjs.add(new THREE.Mesh(geom, material4), longitude + 0.001, latitude); // slightly east
+        const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+        const material2 = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+        const material3 = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+        const material4 = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        arjs.add(new THREE.Mesh(geom, material), longitude, latitude + 0.001); // slightly north
+        arjs.add(new THREE.Mesh(geom, material2), longitude, latitude - 0.001); // slightly south
+        arjs.add(new THREE.Mesh(geom, material3), longitude - 0.001, latitude); // slightly west
+        arjs.add(new THREE.Mesh(geom, material4), longitude + 0.001, latitude); // slightly east
 
-        var root = new THREE.Object3D();
-        scene.add(root);
+
+        // The below code not working yet
+        // var root = new THREE.Object3D();
+        // scene.add(root);
         var threeGLTFLoader = new GLTFLoader();
         var model;
         var mixers = []
@@ -160,7 +162,7 @@ function main() {
             model.position.z = -100;
             console.log("Flamino Added")
         })
-        arjs.add(root, longitude, latitude + 0.001); // slightly north
+        arjs.add(model, longitude, latitude + 0.001); // slightly north
     }
 
 
