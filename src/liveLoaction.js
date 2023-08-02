2e9acb0e4dac86101047bb68a1fa8b793f2db14d
Anish Kumar <anish.kumar@chicmic.co.in>
2023-08-02 14:05:01 +0530

    Added 3d model

--- src/liveLoaction.js
+++ src/liveLoaction.js
@@ -21,8 +21,8 @@ function main() {
     // scene.add(pointLight);
 
     var dLight = new THREE.DirectionalLight("rgb(66, 165, 245, 100)", 1);
-    dlight.position.set(0, 200, 30);
-    dlight.castShadow = true;
+    dLight.position.set(0, 200, 30);
+    dLight.castShadow = true;
     scene.add(dLight);
 
     renderer.setPixelRatio(window.devicePixelRatio);
